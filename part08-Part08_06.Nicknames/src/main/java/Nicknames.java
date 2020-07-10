d1f6a9688793b8ad5c035dfe3b0589464c6943c6
Hari Basnet <hari.basnet@x-akseli.fi>
2020-07-10 15:32:01 +0300

    abbreviations map exercise done

--- part08-Part08_06.Nicknames/src/main/java/Nicknames.java
+++ part08-Part08_06.Nicknames/src/main/java/Nicknames.java
@@ -5,6 +5,15 @@ public class Nicknames {
 
     public static void main(String[] args) {
         // Do the operations required here!
+        
+        HashMap<String, String> nicknamesMap = new HashMap<>();
+        
+        nicknamesMap.put("mathew", "matt");
+        nicknamesMap.put("michael", "mix");
+        nicknamesMap.put("arthur", "artie");
+        
+        System.out.println(nicknamesMap.get("michael"));
+        
     }
 
 }
