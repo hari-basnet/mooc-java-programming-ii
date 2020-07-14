ffcf2943f9b85edea31cfaab56eade1e6aed937d
Hari Basnet <hari.basnet@x-akseli.fi>
2020-07-14 10:36:07 +0300

    Vehicle registry problem done: lesson learned, if unique items needed put into a set

--- part08-Part08_13.VehicleRegistry/src/main/java/VehicleRegistry.java
+++ part08-Part08_13.VehicleRegistry/src/main/java/VehicleRegistry.java
@@ -1,5 +1,9 @@
 
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -19,11 +23,78 @@ public class VehicleRegistry {
     }
     
     public boolean add(LicensePlate licensePlate, String owner){
-        if(!this.registry.containsValue(owner)){
+        
+        if(registry.get(licensePlate) == null){
             this.registry.put(licensePlate, owner);
             return true;
         }
+       
+        for(Map.Entry<LicensePlate, String> entry : registry.entrySet()){
+            if(entry.getKey().equals(licensePlate)){
+                return false;
+            }
+            
+            if(entry.getValue().equals(owner)){
+                return false;
+            }
+        }
+        this.registry.put(licensePlate, owner);
+        return true;
+    }
+    
+    public String get(LicensePlate licensePlate){
+        if(this.registry.keySet().contains(licensePlate)){
+            return this.registry.get(licensePlate);
+        }
         
+        return null;
+    }
+    
+    public boolean remove(LicensePlate licensePlate){
+        if(registry.keySet().contains(licensePlate)){
+            this.registry.remove(licensePlate);
+            return true;
+        }
         return false;
     }
+    
+    public void printLicensePlates(){
+        for(LicensePlate l : registry.keySet()){
+            System.out.println(l.toString());
+        }
+    }
+    
+    public void printOwners(){
+        
+        Set<String> owners = new HashSet<>(registry.values());
+        // alternative solution 
+        // registry.values().stream().distinct().forEach(System.out::println);
+        for(String s : owners){
+            System.out.println(s);
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 41 * hash + Objects.hashCode(this.registry);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final VehicleRegistry other = (VehicleRegistry) obj;
+        return Objects.equals(this.registry, other.registry);
+    }
+    
+    
 }
