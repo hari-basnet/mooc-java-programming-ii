817349170d64cfad84473449128204e44964cb34
Hari Basnet <hari.basnet@x-akseli.fi>
2020-07-16 09:03:19 +0300

    Person class complete/inheritance

--- part09-Part09_02.PersonAndSubclasses/src/main/java/Main.java
+++ part09-Part09_02.PersonAndSubclasses/src/main/java/Main.java
@@ -1,8 +1,22 @@
 
+import java.util.ArrayList;
+
+
 public class Main {
 
     public static void main(String[] args) {
         // write your test code here
+        Student s = new Student("Ollie","6381 Hollywood Blvd. Los Angeles 90028"); 
+        System.out.print(s);
+        
+        Teacher t = new Teacher("Joel Kaasinen","Haagantie 123", 980); 
+        System.out.print(t);
+    }
+    
+    public static void printPersons(ArrayList<Person> persons) {
+        for(Person p : persons){
+            System.out.print(p);
+        }
     }
 
 }
